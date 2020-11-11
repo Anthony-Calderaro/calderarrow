df335b2eda1297d632e74e3b416883609373031d
Anthony <anthony@Anthonys-MacBook-Pro.local>
2020-11-10 16:40:49 -0800

    Added writings page

--- pages/writings.js
+++ pages/writings.js
@@ -1,22 +1,21 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import writingStyles from '../styles/Writing.module.css'
 
 import Header from '../components/Header';
 
-export default function Home() {
+export default function Writing() {
   return (
-    <div className={styles.container}>
+    <div className={writingStyles.container}>
       <Head>
-        <title>Calderarrow</title>
-        {/* <link rel="icon" href="/favicon.ico" />  */}
-        {/* Todo: Replace w/ Custom icon */}
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className={styles.main}>
+      <main className={writingStyles.main}>
 
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={writingStyles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
