4f06e5cb047e1bf7bb5ce7fb0f3f0d6bb3e51c48
Anthony <anthony@Anthonys-MBP.fios-router.home>
2020-12-17 00:47:23 -0500

    Fix text from contact page.

--- pages/contact.js
+++ pages/contact.js
@@ -4,8 +4,9 @@ import styles from '../styles/Home.module.css';
 export default function Projects() {
   return (
     <div>
-      <h1>Our Holistic Approach</h1>
-      <p style={{ lineHeight: '2rem' }}>Our team obtains updated bank data and adds dozens of new accounts and products every day. Unlike other companies[Quora link] or blogs offering financial advice that provide you with a preselected number of options and recommend the best from that pool of choices, so they can receive kickbacks or show more advertisements. We don't serve ads, and we don't exclude any companies or products. We intentionally seek out the most accurate, up-to-date information on as many products as humanly possible, to ensure that you're making a fully informed decision.</p>
+      Coming soon.
+      {/* <h1>Our Holistic Approach</h1> */}
+      {/* <p style={{ lineHeight: '2rem' }}>Our team obtains updated bank data and adds dozens of new accounts and products every day. Unlike other companies[Quora link] or blogs offering financial advice that provide you with a preselected number of options and recommend the best from that pool of choices, so they can receive kickbacks or show more advertisements. We don't serve ads, and we don't exclude any companies or products. We intentionally seek out the most accurate, up-to-date information on as many products as humanly possible, to ensure that you're making a fully informed decision.</p>
       
       <h1>Prioritizing Everyday Customers</h1>
       <p style={{ lineHeight: '2rem' }}>We keep things simple by aligning our incentives with yours to maximize your returns. Unlike household products, comparing financial products can be done objectively. We are able to precisely assess the price and quality of financial products by evaluating the return, price (including fees), and risk. By aggregating as much data as possible for as many products as possible, you know that your finanical decisions are in good hands with bankrank.</p>
@@ -14,7 +15,7 @@ export default function Projects() {
       <p style={{ lineHeight: '2rem' }}>Companies cannot pay-to-win. We do not offer sponsorships, premium partnerships, or advertisements. They compete against each other -- we act as the referree by defining the terms of engagement which align with our customers' needs.</p>
       
       <h1>The End Result</h1>
-      <p style={{ lineHeight: '2rem' }}>We are the world's most comprehensive financial marketplace. We currently have information on X banks representing $Y Assets under management, or Z% of total assets in the united states. In data, we trust.</p>
+      <p style={{ lineHeight: '2rem' }}>We are the world's most comprehensive financial marketplace. We currently have information on X banks representing $Y Assets under management, or Z% of total assets in the united states. In data, we trust.</p> */}
 
     </div>
   );
