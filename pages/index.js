3d9da3c91e7f897451f0bc4652cbf1ba8f91fac9
Anthony <anthony@Anthonys-MBP.fios-router.home>
2020-12-17 02:40:01 -0500

    Updated emoticon and landing page text.

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,11 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div>
-      <h1>I'm Anthony! 👋</h1>
+      <h1>Hi, I'm Anthony! 👋</h1>
       <p>Calderarrow is sort of a phoenitic pronounciation for my last name: Calderaro.</p>
-      <p>I'm an developer with a background in financial accounting (CPA). I'm obsessed with fintech.</p>
-      <p>I am an avid middle-distance runner. You can check out my most recent PRs <Link href='/writings/run-log'><a className={styles.blogLink}>here.</a></Link></p>
+      <p>I'm a software developer and Certified Public Accountant <Link href='https://cpaportal.boa.virginia.gov/Verification/'><a className={styles.blogLink}>(License #50534)</a></Link>. </p>
+      <p>I'm obsessed with fintech and working on <Link href='https://bankrank.io'><a className={styles.blogLink}>bankrank.io</a></Link> in my spare time.</p>
+      <p>I'm an avid middle-distance runner. You can check out my most recent times <Link href='/writings/run-log'><a className={styles.blogLink}>here.</a></Link></p>
       <p>I'm a lifetime fan of the University of Notre Dame. Most of this site is styled according to their <Link href='https://onmessage.nd.edu/university-branding/colors/'><a className={styles.blogLink}>official colors.</a></Link></p>
       <p>This is my personal website where you can read things I've written, check out projects I've worked on, and get in touch with me.</p>
 
