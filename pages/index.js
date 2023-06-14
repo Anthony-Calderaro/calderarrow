568b052c29f4744dd59351dc35a96d3cce6f570f
Anthony Calderaro <ajc-sorin@protonmail.com>
2023-06-13 19:39:00 -0700

    Triggers analytics once.

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,11 @@ import styles from '../styles/Home.module.css';
 const fetcher = url => fetch(url).then(r => r.json());
 
 const HomePageLink = ({ linkObject }) => {
-  const { data, error } = useSWR('/api/hello', fetcher);
-  console.log('data: ', data);
-  console.log('error: ', error);
+  useEffect(() => {
+    const { data, error } = useSWR('/api/hello', fetcher);
+    console.log('data: ', data);
+    console.log('error: ', error);
+  }, [])
   const { link, text, target, ariaLabel } = linkObject;
   return (
     <Link href={link} target={target && target} aria-label={ariaLabel} rel='noreferrer' className={styles.blogLink}>
